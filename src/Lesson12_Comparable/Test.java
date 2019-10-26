e94bb4d42c72a5ed9277b8dd721f78cb3e761495
mrseneca <mrceheka@gmail.com>
2019-10-26 13:26:23 +0300

    Lesson 12 - Comparable

---
+++ src/Lesson12_Comparable/Test.java
@@ -0,0 +1,76 @@
+package Lesson12_Comparable;
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Person> peopleList = new ArrayList<>();
+        Set<Person> peopleSet = new TreeSet<>();
+
+        addElements(peopleList);
+        addElements(peopleSet);
+
+        Collections.sort(peopleList);
+
+        System.out.println(peopleList);
+        System.out.println(peopleSet);
+    }
+
+    private static void addElements(Collection collection) {
+        collection.add(new Person(2, "To"));
+        collection.add(new Person(4, "George"));
+        collection.add(new Person(3, "Katy"));
+        collection.add(new Person(1, "Bob"));
+    }
+}
+
+class Person implements Comparable<Person> {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (this.getName().length() > o.getName().length()) return 1;
+        else if (this.getName().length() < o.getName().length()) return -1;
+        else return 0;
+    }
+}
