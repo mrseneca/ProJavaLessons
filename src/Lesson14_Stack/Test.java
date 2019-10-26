16be039588cf7288abf2ffc7d864cb56c24f5620
mrseneca <mrceheka@gmail.com>
2019-10-26 13:54:13 +0300

    Lesson 14 - Stack

---
+++ src/Lesson14_Stack/Test.java
@@ -0,0 +1,22 @@
+package Lesson14_Stack;
+
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(5);
+        stack.push(3);
+        stack.push(1);
+
+        while (!stack.empty()) {
+            System.out.println(stack.pop());
+        }
+
+//        System.out.println(stack.peek());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+    }
+}
