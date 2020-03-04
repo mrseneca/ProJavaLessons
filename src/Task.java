62d849048662b16ba3a3dca3faee49bd5be4d73a
Aleh Kryvets <mrceheka@gmail.com>
2020-03-04 14:13:17 +0300

    Task for Interview

---
+++ src/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+    public static void main(String[] args) {
+        int a = 1;
+        Integer b = null;
+
+        if (a != b) b = a;
+
+        System.out.println(a+b);
+    }
+}
